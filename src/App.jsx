b34051ea15768dfc173e41650c87d9192f987518
Lrev47 <luis.revilla47@yahoo.com>
2024-07-08 12:30:23 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-// Import the React library, which allows us to define components
+// Import the React library
 import React from "react";
-// Import the Players component, which we'll use to show a list of players
+// Import the Players component
 import Players from "./features/players/Players";
+
 // Define the App component
 function App() {
-  // This component renders the Players component inside a div
-  // This div has a class of 'App', which we could use for styling
   return (
     <>
       <Players />
